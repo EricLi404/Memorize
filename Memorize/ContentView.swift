e46425e4d1297e69903bf3cb0724942e76aa96f8
LiYuyang <liyuyang@huami.com>
2020-06-12 16:35:31 +0800

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Eric Li on 2020/6/12.
+//  Copyright © 2020 Eric Li. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
