fae76fa61401d70c9e04f3ce5eba12e364c01339
LiYuyang <liyuyang@huami.com>
2020-06-13 19:14:25 +0800

    cmt

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,9 +10,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            Text("hello")
+            Text("hello")
+            Button(action: {
+// do nothing
+            }){
+                Text("点击")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+            }
+        .cornerRadius(10)
+        .shadow(radius: 10)
+        }
     }
 }
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
